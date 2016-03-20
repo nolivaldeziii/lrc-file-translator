5ca057b9ec71142c58b96a33f1cca41456691086
Noli Valdez III <nolivaldeziii@gmail.com>
2016-03-21 00:51:24 +0800

    license stuff

--- Lyrics Translation Appender/about.Designer.cs
+++ Lyrics Translation Appender/about.Designer.cs
@@ -53,6 +53,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "about";
             this.Text = "about";
+            this.Load += new System.EventHandler(this.about_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
--- Lyrics Translation Appender/about.cs
+++ Lyrics Translation Appender/about.cs
@@ -2,6 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+/*
+ Lyrics Translator Program
+    Copyright (C) 2012  Noli Valdez III
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +31,11 @@ namespace Lyrics_Translation_Appender
         public about()
         {
             InitializeComponent();
+        }
+
+        private void about_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
